64b77e5cc668bcb73e9dfa5abb8b06d21280cee9
KKharidev <kkharidev@gmail.com>
2022-01-31 22:06:17 +0530

    Sec 3 complete and some size

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,7 +3,7 @@ import style from "./Card.module.css";
 const Card=(props)=>{
     return(
         <div className={style.card}>  
-            {props.icon}
+            <img className={style.img} src={props.icon} />
             <h1>{props.title}</h1>
             <p>{props.content}</p>
         </div>
--- src/sections/section3/Section3.jsx
+++ src/sections/section3/Section3.jsx
@@ -1,57 +1,51 @@
 import style from "./Section3.module.css";
 import Card from "../../components/card/Card";
-import { MdDeveloperMode } from "react-icons/md";
+
+import img1 from "../../assets/graphic-design.png";
+import img2 from "../../assets/branding.png";
+import img3 from "../../assets/wp.png";
+import img4 from "../../assets/content .png";
+import img5 from "../../assets/gd.png";
+import img6 from "../../assets/domain.png";
 
 import Section from "../../components/section/Section.jsx";
 import StylizedWord from "../../components/stylizedWord/StylizedWord";
 
 const Section3 = () => {
-    const icon1 = <MdDeveloperMode />;
 
     const data = [
         {
-            icon: <MdDeveloperMode />,
+            icon: img1,
             title: "asdadsad",
-            content: "askhdsadkja dhakjdha dkadhakjdhad",
+            content: "On The Other Hand, We Denounce With Righteous Indignation And Dislike Men Who Are So Beguiled And Demoralized By The Charms Of Pleasure Of The Moment",
         },
 
         {
-            icon: <MdDeveloperMode />,
+            icon: img2,
             title: "asdadsad",
-            content: "askhdsadkja dhakjdha dkadhakjdhad",
+            content: "On The Other Hand, We Denounce With Righteous Indignation And Dislike Men Who Are So Beguiled And Demoralized By The Charms Of Pleasure Of The Moment",
         },
         {
-            icon: <MdDeveloperMode />,
+            icon: img3,
             title: "asdadsad",
-            content: "askhdsadkja dhakjdha dkadhakjdhad",
-        },
-
-        {
-            icon: <MdDeveloperMode />,
-            title: "asdadsad",
-            content: "askhdsadkja dhakjdha dkadhakjdhad",
+            content: "On The Other Hand, We Denounce With Righteous Indignation And Dislike Men Who Are So Beguiled And Demoralized By The Charms Of Pleasure Of The Moment",
         },
 
         {
-            icon: <MdDeveloperMode />,
+            icon: img4,
             title: "asdadsad",
-            content: "askhdsadkja dhakjdha dkadhakjdhad",
-        },
-        {
-            icon: <MdDeveloperMode />,
-            title: "asdadsad",
-            content: "askhdsadkja dhakjdha dkadhakjdhad",
-        },
-        {
-            icon: <MdDeveloperMode />,
-            title: "asdadsad",
-            content: "askhdsadkja dhakjdha dkadhakjdhad",
+            content: "On The Other Hand, We Denounce With Righteous Indignation And Dislike Men Who Are So Beguiled And Demoralized By The Charms Of Pleasure Of The Moment",
         },
 
         {
-            icon: <MdDeveloperMode />,
+            icon: img5,
             title: "asdadsad",
-            content: "askhdsadkja dhakjdha dkadhakjdhad",
+            content: "On The Other Hand, We Denounce With Righteous Indignation And Dislike Men Who Are So Beguiled And Demoralized By The Charms Of Pleasure Of The Moment",
+        },
+        {
+            icon: img6,
+            title: "asdadsad",
+            content: "On The Other Hand, We Denounce With Righteous Indignation And Dislike Men Who Are So Beguiled And Demoralized By The Charms Of Pleasure Of The Moment",
         },
     ];
     return (
