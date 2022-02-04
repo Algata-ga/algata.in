b7b434f8e445db442d025f3aa096df8e91094064
KKharidev <kkharidev@gmail.com>
2022-02-04 21:08:30 +0530

    Modals page

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,20 @@ import Section2 from "./sections/section2/Section2";
 import Section3 from "./sections/section3/Section3";
 import Section4 from "./sections/section4/Section4";
 import Footer from "./sections/footer/Footer";
+import Modals from "./sections/modal/Modals";
 
 
 function App() {
     return (
         <>
+            <Modals />
             <Navbar />
             <Section1 />
             <Section2 />
             <Section3 />
             <Section4 />
             <Footer />
+
         </>
     );
 }
