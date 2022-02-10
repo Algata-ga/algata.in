a7a567672d8b80efabb686be268c73aa4edc0b13
Alfred Jophy <alfredjophy@protonmail.com>
2022-02-10 05:49:08 +0530

    removed auto popup

--- src/sections/section3/Section3.jsx
+++ src/sections/section3/Section3.jsx
@@ -4,8 +4,6 @@ import Section from "../../components/section/Section.jsx";
 import StylizedWord from "../../components/stylizedWord/StylizedWord";
 import Card from "../../components/card/Card";
 
-import ModalMain from "../modal/ModalMain";
-
 import { useState, useRef } from "react";
 
 import useIntersection from "../../utils/useIntersection";
@@ -13,9 +11,6 @@ import useIntersection from "../../utils/useIntersection";
 import content from "./content";
 
 const Section3 = () => {
-    const ref = useRef();
-    const [isModalShown, setModalShown] = useState(false);
-    const isVisible = useIntersection(ref, "0px");
     return (
         <Section height="auto">
             <div className={style.cards}>
@@ -31,8 +26,6 @@ const Section3 = () => {
             <div>
                 <button className={style.btn}>Explore More</button>
             </div>
-            {isVisible && !isModalShown ? <ModalMain /> : null}
-            <span ref={ref}></span>
         </Section>
     );
 };
