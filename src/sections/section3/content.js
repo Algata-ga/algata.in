daae79d8142efe3b567d95ac13ad16781787be74
Alfred Jophy <alfredjophy@protonmail.com>
2022-02-17 17:53:27 +0530

    added cloud service section

--- src/sections/section3/content.js
+++ src/sections/section3/content.js
@@ -1,49 +1,48 @@
-import img1 from "../../assets/graphic-design.png";
-import img2 from "../../assets/branding.png";
-import img3 from "../../assets/wp.png";
-import img4 from "../../assets/content_.png";
-import img5 from "../../assets/gd.png";
-import img6 from "../../assets/domain.png";
+import Designing from "../../assets/graphic-design.png";
+import Branding from "../../assets/branding.png";
+import ContentWriting from "../../assets/content_.png";
+import Hosting from "../../assets/domain.png";
+import Building from "../../assets/gd.png";
+import CloudServices from "../../assets/cloud_services.png";
 
 const content = [
     {
-        icon: img1,
+        icon: Designing,
         title: "Designing",
         content:
             "Our very creative design team can collaborate with you in live-sessions to design your websites, apps and more and improve upon it with industry-standard UI/UX practices.",
     },
 
     {
-        icon: img2,
+        icon: Branding,
         title: "Branding",
         content:
             "If you starting a new venture, having a brand is very important for you to stand out among the sea of competitors in the modern web. We can help you with creating a positive brand image based on your values, goals and the target demographic.",
     },
     {
-        icon: img3,
-        title: "Graphic Designing",
-        content:
-            "Do you need pretty graphs, designs, backgrounds, characters or anything graphic for your website, the design team would love to hear your thoughs and learn about your project and come up with beautiful graphics for you.",
-    },
-
-    {
-        icon: img4,
+        icon: ContentWriting,
         title: "Content Writing",
         content:
             'Have an existing website or blog? Do you feel like the content in it isn\'t up to the mark? We can help you with adding content to your existing website or a prototype to replace all your "Lorem Ipsum"s.',
     },
 
     {
-        icon: img5,
+        icon: Building,
         title: "Building",
         content:
             "Have a website design that you want to build? Do you like the website we have designed for you? Then we can turn that into a working website. Be it a static website to showcase your buisness, a personal blog where you are constantly adding stories, images and more or a complete web application, we have got you covered.",
     },
     {
-        icon: img6,
+        icon: Hosting,
         title: "Hosting",
         content:
             "We provide a complete solution when it comes to your online presence. We provide servers to host your websites, manage your domain, provide SSL for your website, etc and make sure the servers are as secure as possible.",
+    },
+    {
+        icon: CloudServices,
+        title: "Cloud Solutions",
+        content:
+            "Do you or you company needed private cloud storage, internal management systems, mail services, full-on office suite of applications in the cloud, password management and more. We can provide you all these on a personal server without needing to rely on any enterprises, tailored and priced according to your needs.",
     },
 ];
 
