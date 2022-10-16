e15b342b8363ccae28d655ab2c9d197e69db758b
KKharidev <kkharidev@gmail.com>
2022-10-16 13:49:27 +0530

    Updated Section2

--- src/sections/section2/Section2.jsx
+++ src/sections/section2/Section2.jsx
@@ -1,35 +1,17 @@
 import style from "./Section2.module.css";
-
-import element from "../../assets/element.svg";
-
 import Section from "../../components/section/Section.jsx";
-import StylizedWord from "../../components/stylizedWord/StylizedWord";
-
-import { AiOutlineArrowDown } from "react-icons/ai";
-
+import hero from "../../assets/hero.svg";
 const Section2 = () => {
     return (
         <Section height="auto" padding="5rem">
             <div className={style.sec2}>
                 <div className={style.box}>
-                    <img src={element} className={style.element} alt="" />
+                    <img className={style.hero} src={hero} alt="" />
                 </div>
                 <div className={style.contentbox}>
-                    <h1 className={style.h1}>
-                        have an <StylizedWord text="idea" fontSize="3.2rem" />{" "}
-                        you want to see on the web, <br />
-                        we can help you with that
-                    </h1>
                     <p className={style.p}>
-                        Algata Creative Studio has been helping teams with
-                        having a great online presence with beautiful websites,
-                        marketing campaings, branding and other services. We are
-                        dedicated to making the interweb a beautiful place with
-                        our works.
+                        Algata has been combining design thinking, UI/UX strategy, and senior programming knowledge to assist businesses and startups in creating unique websites and applications.
                     </p>
-                    <button className={style.arrow}>
-                        <AiOutlineArrowDown />
-                    </button>
                 </div>
             </div>
         </Section>
